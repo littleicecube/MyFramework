083a22096b3a01e94ff289bc17ded6f429551dde
wangzeju <wangzeju@dafy.com>
2019-02-15 12:01:23 +0800

    1420005314414200053144

--- src/main/java/com/palace/seeds/spring/transaction/TrasactionTest.java
+++ src/main/java/com/palace/seeds/spring/transaction/TrasactionTest.java
@@ -1,19 +1,10 @@
-package com.palace.seeds.spring.core;
+package com.palace.seeds.spring.transaction;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Configuration
-@EnableTransactionManagement
-@ComponentScan("com.palace.seeds.spring.core")
 public class TrasactionTest {
 	
-	public static void main(String[] args) throws Exception {
+/*	public static void main(String[] args) throws Exception {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(TrasactionTest.class);
 		UserService user = (UserService) ctx.getBean(UserService.class);
 		user.update();
- 	}
+ 	}*/
 }
